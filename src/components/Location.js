9f3112d25397dfd7787683417dbace1fabc211d8
Adil Isani <adil.isani1@gmail.com>
2026-05-08 17:36:52 +0500

    location

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,7 +1,98 @@
+// import { useEffect, useState } from "react";
+// import { toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import Loading from "./Loading/Loading";
+// export function Location({
+//     searchInput,
+//     units,
+//     getWeatherData,
+//     setLatitude,
+//     setLongitude,
+//     latitude,
+//     longitude
+// }) {
+
+//     const [hasLocationAccess, setHasLocationAccess] = useState(false);
+//     const [loading, setLoading] = useState(true);
+
+
+//     const geoLocationErr = () => toast.error("Geolocation is not supported in your environment ", {
+//         toastId: 'error2',
+//         // position: "top-right",
+//         autoClose: 2000,
+//         pauseOnHover: false,
+
+//     });
+//     const cityErr = () => toast.error("Location Denied, Default location updating", {
+//         toastId: 'error3',
+//         // position: "top-right",
+//         autoClose: 3000,
+//         pauseOnHover: false,
+
+//     });
+
+//     useEffect(() => {
+//         const successCallback = (position) => {
+//             setLatitude(position.coords.latitude);
+//             setLongitude(position.coords.longitude);
+//             setHasLocationAccess(true);
+//             setLoading(false);
+//         };
+
+//         const errorCallback = () => {
+//             cityErr();
+//             setLatitude(37.7749);
+//             setLongitude(-122.4194);
+//             setHasLocationAccess(true);
+//             setLoading(false);
+//         };
+
+//         const askForLocationAccess = () => {
+//             if ("geolocation" in navigator) {
+//                 navigator.geolocation.getCurrentPosition(
+//                     successCallback,
+//                     errorCallback,
+//                     { enableHighAccuracy: true }
+//                 );
+//             } else {
+//                 geoLocationErr();
+//             }
+//         };
+
+//         askForLocationAccess();
+//     }, [setLatitude, setLongitude]);
+
+//     useEffect(() => {
+//         let timeoutId;
+//         if (latitude && longitude) {
+//             timeoutId = setTimeout(() => {
+//                 getWeatherData();
+//             }, 1000);
+//         }
+
+//         return () => clearTimeout(timeoutId);
+//     }, [searchInput, units, latitude, longitude, getWeatherData]);
+
+
+//     if (!hasLocationAccess) {
+//         return <div><Loading /></div>;
+//     }
+
+
+//     if (loading) {
+//         return <div><Loading /></div>;
+//     }
+
+//     return <div></div>
+// }
+
+
 import { useEffect, useState } from "react";
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import Loading from "./Loading/Loading";
+
 export function Location({
     searchInput,
     units,
@@ -9,79 +100,104 @@ export function Location({
     setLatitude,
     setLongitude,
     latitude,
-    longitude
+    longitude,
 }) {
 
-    const [hasLocationAccess, setHasLocationAccess] = useState(false);
     const [loading, setLoading] = useState(true);
 
+    // Error Toasts
+    const geoLocationErr = () =>
+        toast.error("Geolocation is not supported in your browser", {
+            toastId: "error2",
+            autoClose: 2000,
+            pauseOnHover: false,
+        });
 
-    const geoLocationErr = () => toast.error("Geolocation is not supported in your environment ", {
-        toastId: 'error2',
-        // position: "top-right",
-        autoClose: 2000,
-        pauseOnHover: false,
+    const cityErr = () =>
+        toast.error("Location denied. Using default location.", {
+            toastId: "error3",
+            autoClose: 3000,
+            pauseOnHover: false,
+        });
 
-    });
-    const cityErr = () => toast.error("Location Denied, Default location updating", {
-        toastId: 'error3',
-        // position: "top-right",
-        autoClose: 3000,
-        pauseOnHover: false,
-
-    });
-
+    // Ask for location access
     useEffect(() => {
+
         const successCallback = (position) => {
-            setLatitude(position.coords.latitude);
-            setLongitude(position.coords.longitude);
-            setHasLocationAccess(true);
+
+            const userLatitude = position.coords.latitude;
+            const userLongitude = position.coords.longitude;
+
+            console.log("Latitude:", userLatitude);
+            console.log("Longitude:", userLongitude);
+
+            setLatitude(userLatitude);
+            setLongitude(userLongitude);
+
             setLoading(false);
         };
 
-        const errorCallback = () => {
+        const errorCallback = (error) => {
+
+            console.log("Geolocation Error:", error);
+
             cityErr();
+
+            // Default location (San Francisco)
             setLatitude(37.7749);
             setLongitude(-122.4194);
-            setHasLocationAccess(true);
+
             setLoading(false);
         };
 
-        const askForLocationAccess = () => {
-            if ("geolocation" in navigator) {
-                navigator.geolocation.getCurrentPosition(
-                    successCallback,
-                    errorCallback,
-                    { enableHighAccuracy: true }
-                );
-            } else {
-                geoLocationErr();
-            }
-        };
+        if ("geolocation" in navigator) {
 
-        askForLocationAccess();
-    }, [setLatitude, setLongitude]);
+            navigator.geolocation.getCurrentPosition(
+                successCallback,
+                errorCallback,
+                {
+                    enableHighAccuracy: true,
+                    timeout: 10000,
+                    maximumAge: 0,
+                }
+            );
 
-    useEffect(() => {
-        let timeoutId;
-        if (latitude && longitude) {
-            timeoutId = setTimeout(() => {
-                getWeatherData();
-            }, 1000);
+        } else {
+
+            geoLocationErr();
+            setLoading(false);
         }
 
-        return () => clearTimeout(timeoutId);
-    }, [searchInput, units, latitude, longitude, getWeatherData]);
+    }, [setLatitude, setLongitude]);
 
+    // Fetch weather data
+    useEffect(() => {
 
-    if (!hasLocationAccess) {
-        return <div><Loading /></div>;
-    }
+        if (latitude != null && longitude != null) {
 
+            const timeoutId = setTimeout(() => {
+                getWeatherData();
+            }, 500);
 
+            return () => clearTimeout(timeoutId);
+        }
+
+    }, [
+        searchInput,
+        units,
+        latitude,
+        longitude,
+        getWeatherData,
+    ]);
+
+    // Loading Screen
     if (loading) {
-        return <div><Loading /></div>;
+        return (
+            <div>
+                <Loading />
+            </div>
+        );
     }
 
-    return <div></div>
+    return null;
 }
